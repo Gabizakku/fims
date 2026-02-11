476101dfcdce30f1d59fbb566d7b2572dee4040d
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-12 03:50:36 +0800

    feat(`LogOutButton`): add logout functionality

--- src/routes/(ui)/LogOutButton.svelte
+++ src/routes/(ui)/LogOutButton.svelte
@@ -1,3 +1,20 @@
-<button class="border-2 border-fims-red bg-white text-fims-red px-9 py-2 hover:bg-fims-red hover:text-white disabled:border-fims-gray disabled:text-fims-gray rounded-full">
+<script>
+  import { invalidateAll } from "$app/navigation";
+</script>
+
+<button
+  type="submit"
+  class="border-2 border-fims-red bg-white text-fims-red px-9 py-2 hover:bg-fims-red hover:text-white disabled:border-fims-gray disabled:text-fims-gray rounded-full"
+  onclick={async () => {
+    await fetch('/logout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+
+    invalidateAll();
+  }}
+>
   Log-out
 </button>
