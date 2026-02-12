da9ea4182a6ab241063caa289db27bf5fa6ffd5e
nousername010 <gdemail2025@proton.me>
2026-02-12 20:51:50 +0800

    chore: develop tests

---
+++ tests/account.test.ts
@@ -0,0 +1,15 @@
+import { expect, describe, beforeEach, beforeAll, afterEach, afterAll } from "vitest"
+import { db } from "../src/lib/server/db"
+import { user } from "../src/lib/server/db/schema"
+import { eq } from "drizzle-orm"
+import * as AccountDAO from "../src/lib/server/accountDAO"
+
+describe("account listing", async () => {
+    let accounts = await AccountDAO.listAll()
+    expect(accounts.length).toBeGreaterThan(0)
+    expect(accounts.find((elem) => {elem.accountId == "oP77pgWyY6fcUjyanDUbyPOWOr9eNSt1"})).not.toBe([]);
+});
+
+describe("account creation and deletion", async () => {
+
+});
