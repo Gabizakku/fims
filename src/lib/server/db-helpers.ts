52703754e6ff7f4a5ece6b7efac553208313f917
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-15 13:38:38 +0800

    fix(`db`): adhere to `returning` return type

--- src/lib/server/db-helpers.ts
+++ src/lib/server/db-helpers.ts
@@ -25,9 +25,9 @@ export async function logChange(makerid: string, tupleid: number, operation: str
             tupleid,
             operation,
         })
-        .returning({ id: changelog.logid });
+        .returning();
 
-    const [{ id: logid }, _] = logids;
+    const [{ logid }, _] = logids;
 
     return logid;
 }
@@ -40,10 +40,10 @@ export async function makeUser(makerid: string, id: string, role: string) {
             userid: id,
             role,
         })
-        .returning({ id: userinfo.userinfoid });
+        .returning();
 
     // Log!
-    const [{ id: tupleid }, _] = returnedIds;
+    const [{ userinfoid: tupleid }, _] = returnedIds;
 
     const logid = await logChange(makerid, tupleid, 'Made account.');
 
